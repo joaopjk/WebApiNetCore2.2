28c9e34d447131c13ae7e43e21023a43102bc9fb
Joaopjk <joaopjk48@gmail.com>
2020-02-17 00:07:32 -0300

    Component Eventos

---
+++ ProAgilApp/src/app/Eventos/Eventos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector:  'app-Eventos',
+  templateUrl: './Eventos.component.html',
+  styleUrls: ['./Eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+  eventos: any ;
+  constructor(private http: HttpClient ) { }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+  getEventos() {
+     this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {this.eventos = response; }, error  =>  {
+        console.log(error);
+      });
+  }
+}
