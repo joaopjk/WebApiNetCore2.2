6d2ea9e2db305297e07333cc5204c9437f6da337
Joaopjk <joaopjk48@gmail.com>
2020-02-22 07:42:52 -0300

    Adcionando Imagens/Grid

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -54,6 +54,7 @@ namespace ProAgil.API
             }
             app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
--- ProAgilApp/src/app/Eventos/Eventos.component.ts
+++ ProAgilApp/src/app/Eventos/Eventos.component.ts
@@ -8,11 +8,17 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./Eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  eventos: any ;
+  eventos: any = [] ;
+  imagemLargura = 50;
+  imagemMargen = 2;
+  mostrarImagem = false;
   constructor(private http: HttpClient ) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+  alternarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
   }
   getEventos() {
      this.http.get('http://localhost:5000/api/values').subscribe(
